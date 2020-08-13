070cfafd7e8297e20418b444387e19505d6ea5a1
Jaime Sanders <jaimelyn517@gmail.com>
2020-08-12 17:32:08 -0700

    adds users to contacts and reseeds the database

--- database/seed.js
+++ database/seed.js
@@ -1,12 +1,20 @@
 const chance = require('chance').Chance();
 const Contact = require('../lib/models/Contact');
 const Communication = require('../lib/models/Communication');
+const User = require('../lib/models/User');
 
-module.exports = async({ contacts = 10, communications = 25 } = {}) => {
+module.exports = async({ users = 10, contacts = 10, communications = 25 } = {}) => {
 
   const communicationTypes = ['LinkedIn', 'email', 'phone', 'slack'];
 
+  const createdUsers = await User.create([...Array(users)].map((_, i) => ({
+    email: `test${i}@email.com`,
+    password: 'password',
+    profileImage: 'https://fillmurray.com/200/300'
+  })));
+
   const createdContacts = await Contact.create([...Array(contacts)].map(() => ({
+    user: chance.pickone(createdUsers)._id,
     name: chance.name(),
     communicationMedium: chance.pickone(communicationTypes),
     imageUrl: 'https://placekeanu.com/200/300'
